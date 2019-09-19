101cbdb58759871bad91dd92f8110423b2d391ab
Dennis Vroegop <dennis@vroegop.org>
2019-09-19 14:21:22 +0200

    Adds the other patterns

--- Samples/03_Patterns.cs
+++ Samples/03_Patterns.cs
@@ -54,14 +54,46 @@ public partial class Tester
         };
 
         #endregion
+
         return result;
     }
+    #region Property switch
 
+    public float CalcBTW(BtwType btwType) => btwType switch
+    {
+        BtwType.Low => 9.0f,
+        BtwType.High => 21.0f,
+        _ => throw new ArgumentOutOfRangeException(nameof(btwType))
+    };
 
+    #endregion
 
+    #region Tuple switch
+    public string RockPaperScissors(string first, string second)
+    => (first, second) switch
+    {
+        ("rock", "paper") => "rock is covered by paper. Paper wins.",
+        ("rock", "scissors") => "rock breaks scissors. Rock wins.",
+        ("paper", "rock") => "paper covers rock. Paper wins.",
+        ("paper", "scissors") => "paper is cut by scissors. Scissors wins.",
+        ("scissors", "rock") => "scissors is broken by rock. Rock wins.",
+        ("scissors", "paper") => "scissors cuts paper. Scissors wins.",
+        (_, _) => "tie"
+    };
 
+    #endregion
+
+    #region Positional
+    public Hue GetHue(RgbColor color) => color switch
+    {
+        var (r, g, b) when r < 0.2 && g < 0.2 && b < 0.2 => Hue.Dark,
+        var (r, g, b) when r < 0.6 && g < 0.6 && b < 0.6 => Hue.Medium,
+        _ => Hue.Light
+
+    };
+    #endregion
 }
-}
+
 
 public enum Rainbow
 {
@@ -76,8 +108,37 @@ public enum Rainbow
 
 public class RgbColor
 {
+    public float R { get; set; }
+    public float G { get; set; }
+    public float B { get; set; }
+
     public RgbColor(float r, float g, float b)
     {
-
+        R = r;
+        G = g;
+        B = b;
     }
+
+    #region Deconstruct
+    public void Deconstruct(out float r, out float g, out float b)
+    {
+        r = R;
+        g = G;
+        b = B;
+    }
+    #endregion
 }
+
+public enum BtwType
+{
+    Low,
+    High
+}
+
+public enum Hue
+{
+    Light,
+    Medium,
+    Dark;
+}
+
