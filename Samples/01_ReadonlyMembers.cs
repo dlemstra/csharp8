d96b6d64936706da9593b282447b88e238fb0a69
Dennis Vroegop <dennis@vroegop.org>
2019-09-19 13:50:14 +0200

    Add demo ReadOnly

--- Samples/01_ReadonlyMembers.cs
+++ Samples/01_ReadonlyMembers.cs
@@ -1,1 +1,36 @@
-﻿
+﻿using System;
+
+public partial class Tester
+{
+    public void TestOne()
+    {
+        var l1 = new Line();
+        l1.X1 = 1.0;
+        l1.X2 = 2.0;
+        l1.Y1 = 3.0;
+        l1.Y2 = 4.0;
+
+        var length = l1.Length;
+        Console.WriteLine($"The length of the line is {length}.");
+
+    }
+
+}
+
+public struct Line
+{
+    public double X1 { get; set; }
+    public double Y1 { get; set; }
+    public double X2 { get; set; }
+    public double Y2 { get; set; }
+
+    public double Length
+    {
+        get { return Math.Sqrt(Math.Pow((X2 - X1), 2.0) + Math.Pow((Y2 - Y1), 2.0)); }
+    }
+
+    public override string ToString()
+    {
+        return $"{Length}";
+    }
+}
