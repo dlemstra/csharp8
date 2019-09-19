d8477a1587e8c9c67e895bfbd537624d0b5cb4ae
Dennis Vroegop <dennis@vroegop.org>
2019-09-19 14:33:44 +0200

    Add demo disposable ref structs

--- Samples/06_DisposableRefStructs.cs
+++ Samples/06_DisposableRefStructs.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.IO;
+
+public partial class Tester
+{
+
+    public void TestSix()
+    {
+       
+    }
+}
+
+struct SomeInterface 
+{
+    int SomeNumber { get;set }
+    StreamWriter stream { get; set; }
+
+ 
+}
+
