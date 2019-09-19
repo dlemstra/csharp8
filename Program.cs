a96db81cd6a49c9e02670954400aeb19f6a22378
Dennis Vroegop <dennis@vroegop.org>
2019-09-19 15:11:28 +0200

    Adds Async streams

--- Program.cs
+++ Program.cs
@@ -1,15 +1,23 @@
-﻿namespace csharp8
+﻿using System;
+
+namespace csharp8
 {
     class Program
     {
         static void Main(string[] args)
         {
             Tester tester = new Tester();
-            tester.TestOne();
+            //tester.TestOne();
 
-            tester.TestTwo();
+            //tester.TestTwo();
 
-            tester.TestSeven();
+            //tester.TestSeven();
+            tester.TestEight();
+
+
+            Console.WriteLine("All done...");
+            Console.ReadLine();
+
         }
     }
 }
--- Samples/08_AsynchronousStreams.cs
+++ Samples/08_AsynchronousStreams.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public partial class Tester
+{
+    public async void TestEight()
+    {
+        await foreach (var number in GenerateSequence())
+        {
+            Console.WriteLine(number);
+        }
+
+        // By default implemented in 
+    }
+
+    public async IAsyncEnumerable<int> GenerateSequence()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            await Task.Delay(100);
+            yield return i;
+        }
+    }
+}
