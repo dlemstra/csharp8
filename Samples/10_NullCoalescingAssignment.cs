a101ff8f29c120a17100aac8a2e3388e35ab3367
Dennis Vroegop <dennis@vroegop.org>
2019-09-19 15:23:38 +0200

    Adds unmanagedconstructed types demo

--- Samples/10_NullCoalescingAssignment.cs
+++ Samples/10_NullCoalescingAssignment.cs
@@ -1,4 +1,18 @@
-﻿public partial class Tester
-{
+﻿using System;
+using System.Collections.Generic;
 
+public partial class Tester
+{
+    public void TestTen()
+    {
+        List<int> numbers = null;
+        int? i = null;
+
+        numbers ??= new List<int>();
+        numbers.Add(i ??= 17);
+        numbers.Add(i ??= 20);
+
+        Console.WriteLine(string.Join(' ', numbers));  // output: 17 17
+        Console.WriteLine(i);  // output: 17
+    }
 }
--- Samples/11_UnmanagedConstructedTypes.cs
+++ Samples/11_UnmanagedConstructedTypes.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+
+public partial class Tester
+{
+    public void TestEleven()
+    {
+        Span<Coords<int>> coordinates = stackalloc[]
+        {
+            new Coords<int> { X = 0, Y = 0 },
+            new Coords<int> { X = 1, Y = 0 },
+            new Coords<int> { X = 0, Y = 1 }
+        };
+
+
+    }
+}
+
+public struct Coords<T>
+{
+    public T X;
+    public T Y;
+}
