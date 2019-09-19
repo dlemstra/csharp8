b2accb63f8278564ef2d38b7ec87936645f68e6c
Dennis Vroegop <dennis@vroegop.org>
2019-09-19 14:24:45 +0200

    Adds using demos

--- Samples/04_UsingDeclarations.cs
+++ Samples/04_UsingDeclarations.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using System.IO;
+
+public partial class Tester
+{
+    public void TestFour()
+    {
+
+        #region Old Style
+
+        var file1 = new StreamWriter("MyFile.txt");
+        try
+        {
+            // Do something
+        }
+        finally
+        {
+            file1.Dispose();
+        }
+        #endregion
+
+        #region Slightly better style
+        using (var file2 = new StreamWriter("MyFile.txt"))
+        {
+            // Do something...
+
+            // Dispose happens here!
+        }
+
+        #endregion
+
+        #region New style
+
+        using var file3 = new StreamWriter("MyFile.txt");
+
+        // Do something
+
+        // Dispose happens here, at the end of the enclosing scope
+        #endregion
+    }
+}
